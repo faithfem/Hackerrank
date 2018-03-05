097bfac474189647688fa2019ef51fdfe218e849
faithfem <fmugure@intracitygeeks.org>
2018-03-04 21:35:08 -0500

    first commit

---
+++ Strings/src/Strings.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Strings {
+
+    public static void main (String[]args){
+        Scanner gonyo = new Scanner (System.in);
+        String A = gonyo.next("Hello World");
+        String B = gonyo.next("How you doing?");
+
+        
+
+        System.out.println(A + B);
+    }
+}
