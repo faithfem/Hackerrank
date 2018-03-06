dc02454d1b24263e6ed3775d9dc2dda59de67ad3
faithfem <fmugure@intracitygeeks.org>
2018-03-05 19:10:35 -0500

    strings

--- Strings/src/Strings.java
+++ Strings/src/Strings.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
+
 public class Strings {
 
-    public static void main (String[]args){
-        Scanner gonyo = new Scanner (System.in);
-        String A = gonyo.next("Hello World");
-        String B = gonyo.next("How you doing?");
+    public static void main(String[]args){
 
+        Scanner gonyo = new Scanner(System.in);
+        String A = "Hello World!";
+        int length = A.length();
 
-
-        System.out.println("hello world");
+        System.out.println("Length of String A is " + length);
     }
+
 }
