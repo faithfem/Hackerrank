c7ccd1c3755a9dbd4e417fa83b8901e242099206
faithfem <fmugure@intracitygeeks.org>
2018-03-05 19:28:06 -0500

    strings

--- Strings/src/Strings.java
+++ Strings/src/Strings.java
@@ -1,14 +1,35 @@
-import java.util.Scanner;
-
 public class Strings {
 
     public static void main(String[]args){
 
-        Scanner gonyo = new Scanner(System.in);
+        //Scanner gonyo = new Scanner(System.in);
         String A = "Hello World!";
+        String B = "Hello Java";
         int length = A.length();
+        int lengthB = B.length();
 
         System.out.println("Length of String A is " + length);
+        System.out.println("Length of String B is " + lengthB);
+
+        //1. SUM THE LENGTHS OF A & B
+        //2. FIND THE LONGER STRING
+        //3. CAPITALIZE THE FIRST LETTER IN A & B
+        //4. PRINT THE CAPITALIZED FIRST LETTERS, SEPARATED BY A SPACE
+
+
+        //1.Sum lengths of A & B
+        System.out.println(A.length() + B.length());
+
+        //2. Which is longer, A or B?
+        int result = A.compareTo(B);
+
+        if(result>0)
+            System.out.println("A is larger than B");
+        else if(result<0)
+            System.out.println("A is smaller than B");
+        else
+            System.out.println("A is equal to B");
+
     }
 
 }
